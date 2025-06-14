baff818ec2906064838fd5ea201d5940cac1dd00
himansa2005 <himansadenith@gmail.com>
2025-06-14 22:44:04 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# Student Info Collector
+
+A beginner Python program that collects user input and displays a student profile.
+
+## What I Learned
+- How to use input() in Python
+- String formatting
+- Running Python scripts in VS Code
+
+## How to Run
+1. Open terminal
+2. Run: `python student_info.py`
---
+++ main.py
@@ -0,0 +1,12 @@
+# Student info collector
+
+print("Welcome to the Student Info Collector!")
+name = input("Enter Your Name: ")
+age = input("Enter your age: ")
+school = input("Enter your school name: ")
+
+
+print("\n---Student profile---")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"School: {school}")
